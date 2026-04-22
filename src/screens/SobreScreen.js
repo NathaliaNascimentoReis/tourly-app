adf54272165feaf3554902b0a8fbb81f5144e6a5
Nath <nathalia.n.reis@aluno.senai.br>
2026-04-21 22:01:02 -0300

    feat: finaliza página Sobre

--- src/screens/SobreScreen.js
+++ src/screens/SobreScreen.js
@@ -10,6 +10,29 @@ export default function Sobre() {
                     <Text style={styles.titulo}>Sobre</Text>
                     <Text style={styles.introducaoTexto}>{textoIntroducao.texto}</Text>
                     <Text style={styles.missaoTexto}>{textoMissao.texto}</Text>
+
+                    <View style={styles.div}>
+                        <View style={styles.textos}>
+                            <Text style={styles.textosTitulo}>Locais selecionados:</Text>
+                            <Text style={styles.textosDescricao}>
+                                Atrações turísticas cuidadosamente selecionadas com imagens de alta
+                                qualidade.
+                            </Text>
+                        </View>
+                        <View style={styles.textos}>
+                            <Text style={styles.textosTitulo}>Guia de jantar:</Text>
+                            <Text style={styles.textosDescricao}>
+                                Uma lista completa dos melhores restaurantes e estabelecimentos
+                                gastronômicos locais.
+                            </Text>
+                        </View>
+                        <View style={styles.textos}>
+                            <Text style={styles.textosTitulo}>Navegação Inteligente:</Text>
+                            <Text style={styles.textosDescricao}>
+                                Interface simples e intuitiva com dados de localização detalhados.
+                            </Text>
+                        </View>
+                    </View>
                 </View>
             </ScrollView>
         </View>
@@ -42,6 +65,30 @@ const styles = StyleSheet.create({
         fontWeight: 600,
         fontSize: 18,
         color: '#6F4074',
+        flexWrap: 'wrap',
+    },
+    div: {
+        backgroundColor: '#D9C3D8',
+        borderRadius: 10,
+        padding: 10,
+        gap: 20,
+        marginBottom: 30
+    },
+    textos: {
+        padding: 10,
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+    },
+    textosTitulo: {
+        fontWeight: 'bold',
+        fontSize: 18,
+        width: '50%',
+        paddingRight: 15,
+    },
+    textosDescricao: {
+        flex: 1,
+        fontSize: 15,
+        lineHeight: 22,
         flexWrap: 'wrap',
     },
 });
